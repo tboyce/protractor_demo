73926b8f017434b4a2f8847c398fab72ac9ed710
Timothy Boyce <timothy.boyce@mmgins.com>
2015-07-19 19:03:16 -0400

    load console plugin for protractor

--- protractor.conf.js
+++ protractor.conf.js
@@ -2,13 +2,15 @@ exports.config = {
   seleniumServerJar: './node_modules/protractor/selenium/selenium-server-standalone-2.45.0.jar',
   framework: 'cucumber',
   specs: ['features/*.feature'],
-  multiCapabilities: [{
-    browserName: 'firefox'
-  }, {
+  capabilities: {
     browserName: 'chrome'
-  }],
+  },
   cucumberOpts: {
     require: '.tmp/step_definitions/*_steps.js',
     format: 'pretty'
-  }
+  },
+  plugins: [{
+    path: 'node_modules/protractor/plugins/console',
+    exclude: ['favicon']
+  }]
 };
