81856958633f7f00ec74682aeef43939821d4255
tboyce <timothy.boyce@mmgins.com>
2015-07-21 01:26:38 +0000

    watch should compile first

--- gulp/watch.js
+++ gulp/watch.js
@@ -4,7 +4,7 @@ var gulp = require('gulp');
 var path = require('path');
 var conf = require('./conf');
 
-gulp.task('watch', function () {
+gulp.task('watch', ['compile'], function () {
 
   var paths = [
     path.join(conf.paths.e2e, '/**/*.ts')
